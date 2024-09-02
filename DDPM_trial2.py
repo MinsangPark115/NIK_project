78a0197230c47f95a5d29df7f609bc2248504338
MinsangPark115 <pagemu@gmail.com>
2024-09-02 16:41:46 +0900

    several changes

--- DDPM_trial2.py
+++ DDPM_trial2.py
@@ -1096,13 +1096,13 @@ class DDP:
 
         # save_image(grid, os.path.join(self.conf.sample_dir, f'generated_images_{epoch}.png'))
         print(self.sample['progressive_samples'].reshape(-1, 3, self.conf.dataset.resolution, self.conf.dataset.resolution).shape)
-        grid = make_grid(sample['progressive_samples'].reshape(-1, 3, self.conf.dataset.resolution, self.conf.dataset.resolution), nrow=20)
+        grid_progressive = make_grid(sample['progressive_samples'].reshape(-1, 3, self.conf.dataset.resolution, self.conf.dataset.resolution), nrow=20)
         
         save_image_pil(grid_progressive, os.path.join(self.conf.sample_dir, f'progressive_generated_images_{epoch}.png'))
 
         # save_image(grid, os.path.join(self.conf.sample_dir, f'progressive_generated_images_{epoch}.png'))
 
-def save_image_pil(tensor, file_path, file_name):
+def save_image_pil(tensor, file_path):
     tensor = tensor.clone().cpu()
     if tensor.min() < 0 or tensor.max() > 1:
         tensor = (tensor - tensor.min()) / (tensor.max() - tensor.min())
@@ -1112,7 +1112,7 @@ def save_image_pil(tensor, file_path, file_name):
     np_image = tensor.numpy().transpose(1, 2, 0)
     
     image = Image.fromarray(np_image)
-    image.save(f"{file_path}/{file_name}")
+    image.save(file_path)
 
 class obj(object):
     """
