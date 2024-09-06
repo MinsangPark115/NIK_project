19f2e0e04ef7f208d57338e435705304c6a20c33
MinsangPark115 <pagemu@gmail.com>
2024-09-06 13:34:43 +0900

    add eager mode sampling code as sampling_eager

--- DDPM_sampling_eager.py
+++ DDPM_sampling_eager.py
@@ -1110,7 +1110,7 @@ class DDP:
             sample = progressive_samples_fn(self.ema, self.diffusion, shape, device=device)
             
             for i in range(current_batch_size):
-                image_tensor = sample['samples'][i].cpu().unsqueeze(0)  
+                image_tensor = sample['samples'][i].cpu()
                 file_name = os.path.join(self.conf.sample_dir, f'sample_{total_samples_saved + i}.png')
                 save_image_pil(image_tensor, file_name)
             
